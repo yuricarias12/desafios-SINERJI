7f0a84615ac8e1d72339d1f9e260443832329103
Yuri <yuristricken7@gmail.com>
2024-06-16 18:23:08 -0300

    Melhoria no codigo do desafio, foi acrescentado algumas assertivas

--- Desafios/testes-de-aplicacoes-modernas-com-cypress/InserindoContas/desafio-inserir-conta.cy.js
+++ Desafios/testes-de-aplicacoes-modernas-com-cypress/InserindoContas/desafio-inserir-conta.cy.js
@@ -4,22 +4,22 @@ describe('Colocando conhecimento em pratica', () => {
     beforeEach(() => {
         cy.visit('https://barrigareact.wcaquino.me/');
 
-        cy.get('[data-test="email"]').type('yuri@hotmail.com')
+        cy.get('[data-test="email"]').type('yuri12@hotmail.com')
         cy.get('[data-test="passwd"]').type(2123)
         cy.get('.btn').click()
     })
 
     it('Inserindo Conta', () => {
         // Primeiro, clique no elemento do menu
-        cy.xpath('//li/a[@data-test="menu-settings"]').click();
+        cy.xpath('//li/a[@data-test="menu-settings"]').click()
 
         // Em seguida, clico no link de contas após garantir que está visível
-        cy.xpath('//li//div//a[@href="/contas"]').should('be.visible').click();
+        cy.xpath('//li//div//a[@href="/contas"]').should('be.visible').and('have.text', 'Contas').click();
 
         cy.get('[data-test="nome"]').type('Conta Vet')
        
         cy.get('.btn').click()
 
-        cy.get('.toast-success').should('contain', 'Conta inserida com sucesso!')
+        cy.xpath('//div[contains(@class, "toast toast-success animated bounceIn")]/div[contains(text(), "Conta inserida com sucesso!")]').should('contain', 'Conta inserida com sucesso!')
     })
 })
